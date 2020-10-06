421919e6edaa7f7b928d990eff8391f4c80620a9
YC <yc0015139@gmail.com>
2020-10-06 08:12:45 +0800

    WIP

--- app/src/main/java/com/ycdev/myapplication/utils/ImageLoader.java
+++ app/src/main/java/com/ycdev/myapplication/utils/ImageLoader.java
@@ -3,6 +3,7 @@ package com.ycdev.myapplication.utils;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Handler;
+import android.util.LruCache;
 import android.widget.ImageView;
 
 import org.jetbrains.annotations.NotNull;
@@ -20,6 +21,14 @@ public class ImageLoader {
     private static ImageLoader instance;
     private Handler uiHandler = new Handler();
 
+    private int runtimeMaxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+    private LruCache lruCache = new LruCache<String, Bitmap>(runtimeMaxMemory) {
+        @Override
+        protected int sizeOf(String key, Bitmap value) {
+            return value.getByteCount();
+        }
+    };
+
     public static ImageLoader getInstance() {
         if (instance == null) {
             instance = new ImageLoader();
@@ -28,6 +37,16 @@ public class ImageLoader {
     }
 
     public void loadImageByUrl(ImageView imageView, String url) {
+        Bitmap readBitmap = (Bitmap) lruCache.get(url);
+
+        if (readBitmap == null) {
+            sendRequestAndUpdate(imageView, url);
+        } else {
+            updateImageViewByBitmap(imageView, readBitmap);
+        }
+    }
+
+    private void sendRequestAndUpdate(ImageView imageView, String url) {
         HttpHelper.sendRequest(url, new Callback() {
             @Override
             public void onFailure(@NotNull Call call, @NotNull IOException e) {
@@ -37,10 +56,8 @@ public class ImageLoader {
             @Override
             public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
                 Bitmap bitmap = getBitmapFromResponse(response);
-
-                uiHandler.post(() -> {
-                    imageView.setImageBitmap(bitmap);
-                });
+                lruCache.put(url, bitmap);
+                updateImageViewByBitmap(imageView, bitmap);
             }
         });
     }
@@ -50,5 +67,11 @@ public class ImageLoader {
         InputStream inputStream = responseBody.byteStream();
         BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
         return BitmapFactory.decodeStream(bufferedInputStream);
+    }
+
+    private void updateImageViewByBitmap(ImageView imageView, Bitmap bitmap) {
+        uiHandler.post(() -> {
+            imageView.setImageBitmap(bitmap);
+        });
     }
 }
